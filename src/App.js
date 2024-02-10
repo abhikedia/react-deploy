b7ed4277525d5088d751cdfc7b9d58b769783fa2
Biruk Kebede <biruk@insa.gov.et>
2024-02-10 21:11:36 +0300

    edit href link

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.insa.gov.et/"
           target="_blank"
           rel="noopener noreferrer"
         >
